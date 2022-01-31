3d1786e7a511ebfc400e158619706c6784031534
a3nath <amarn.sks@gmail.com>
2022-01-30 19:31:07 -0500

    changed to class views

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.starting_page.as_view(), name="starting-page"),
-    path("posts/", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_det, name="post-detail-page")
+    path("posts/", views.posts.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.post_det.as_view(), name="post-detail-page")
 ]
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from datetime import date
 from .models import Post, Author, Tag
-from django.views.generic import ListView
-
-#dummy data
-
-
-# fcn to get date
-
-#  views
+from django.views.generic import ListView, DetailView
 
 
 ##list view
 class starting_page(ListView):
     template_name = "blogApp/index.html"
     queryset = Post.objects.order_by("-date")[:2]
+    ##name to refer the output in the template
     context_object_name = "posts"
 
     # def get_query_set(self):
@@ -30,17 +24,26 @@ class starting_page(ListView):
 
 # for our post url
 #list view
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blogApp/posts.html", {'posts': all_posts})
+class posts(ListView):
+    template_name = "blogApp/posts.html"
+    queryset = Post.objects.order_by("-date")
+    context_object_name = "posts"
+
+# def posts(request):
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request, "blogApp/posts.html", {'posts': all_posts})
 
 #for an individual post
 #detail view
-def post_det(request, slug):
-    selectedPost = get_object_or_404(Post, slug=slug)
-    return render(request, "blogApp/post-detail.html", 
-        {'post':selectedPost,
-        'post_tags': selectedPost.tag.all()})
 
+class post_det(DetailView):
+    template_name = 'blogApp/post-detail.html'
+    # queryset = Post.objects.filter(slug=slug)
+    model = Post
+    context_object_name = 'post'
 
-
+# def post_det(request, slug):
+#     selectedPost = get_object_or_404(Post, slug=slug)
+#     return render(request, "blogApp/post-detail.html", 
+#         {'post':selectedPost,
+#         'post_tags': selectedPost.tag.all()})
