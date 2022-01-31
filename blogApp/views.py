368edeba0b8e3a44c7febac86ca34e2c80417750
a3nath <amarn.sks@gmail.com>
2022-01-31 12:54:46 -0500

    updated class views - tags

--- blogApp/views.py
+++ blogApp/views.py
@@ -5,42 +5,34 @@ from django.views.generic import ListView, DetailView
 
 
 ##list view
-class starting_page(ListView):
+class StartingPageView(ListView):
     template_name = "blogApp/index.html"
     queryset = Post.objects.order_by("-date")[:2]
     ##name to refer the output in the template
     context_object_name = "posts"
 
-    # def get_query_set(self):
-    #     base_query = super().get_queryset()
-    #     data = base_query.order_by("-date")[:1]
-    #     return data
-
-
-# def starting_page(request):
-#     all_posts = Post.objects.all().order_by("-date")
-#     latestPost = all_posts[:2]
-#     return render(request, "blogApp/index.html", {'posts':latestPost})
 
 # for our post url
 #list view
-class posts(ListView):
+class AllPostView(ListView):
     template_name = "blogApp/posts.html"
     queryset = Post.objects.order_by("-date")
-    context_object_name = "posts"
+    context_object_name = "all_posts"
 
-# def posts(request):
-#     all_posts = Post.objects.all().order_by("-date")
-#     return render(request, "blogApp/posts.html", {'posts': all_posts})
 
 #for an individual post
 #detail view
 
-class post_det(DetailView):
+class PostDetalView(DetailView):
     template_name = 'blogApp/post-detail.html'
-    # queryset = Post.objects.filter(slug=slug)
     model = Post
-    context_object_name = 'post'
+    context_object_name = 'post'      
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_tags'] = self.object.tag.all()
+        return context
+    
 
 # def post_det(request, slug):
 #     selectedPost = get_object_or_404(Post, slug=slug)
